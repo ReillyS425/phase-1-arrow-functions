5a0b9a0b2911f2c2a279837ab6fda7632eb0f19b
Reilly Shofner <reilly.shofner@gmail.com>
2024-03-13 18:57:31 -0700

    complete phase-1-arrow-functions

---
+++ index.js
@@ -0,0 +1,14 @@
+//function divide(number1=2000, number2=100) {
+ //number1 / number2
+//}
+
+//divide();
+function divide() {
+ return   2000 / 100
+}
+
+divide();
+
+const square = number => number * number;
+
+const add = (number1, number2) => number1 + number2;
